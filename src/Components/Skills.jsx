d430d97cee3f23e324f296fc26b8ff41883f0ce3
Alom Wisdom <ogwisdom27@gmail.com>
2023-09-18 00:50:05 +0100

    chamged the structure of my skill components and adding some images

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,26 +1,36 @@
-import React, { useEffect } from 'react';
+import { Button } from "react-bootstrap";
+import React, { useEffect } from "react";
+import html from "../../src/assets/html.jpg"
+import cssImg from "../../src/assets/css.png"
+import git from "../../src/assets/gitpng.png"
+import react from "../../src/assets/react2.png"
+import tailwind from "../../src/assets/tailwind.png"
+import javascript from "../../src/assets/javascript.png"
+import flask from "../../src/assets/flask.png"
+import fastApi from "../../src/assets/fastApi.png"
+
 
 export const Skills = () => {
-  useEffect(() => {
-    const handleScroll = () => {
-      const skillsContent = document.querySelector('.skills-content');
-      if (skillsContent) {
-        const skillsContentOffset = skillsContent.offsetTop - (window.innerHeight * 0.8);
-        if (window.pageYOffset > skillsContentOffset) {
-          const progressBars = document.querySelectorAll('.progress .progress-bar');
-          progressBars.forEach((bar) => {
-            const width = bar.getAttribute('aria-valuenow') + '%';
-            bar.style.width = width;
-          });
-        }
-      }
-    };
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const skillsContent = document.querySelector('.skills-content');
+  //     if (skillsContent) {
+  //       const skillsContentOffset = skillsContent.offsetTop - (window.innerHeight * 0.8);
+  //       if (window.pageYOffset > skillsContentOffset) {
+  //         const progressBars = document.querySelectorAll('.progress .progress-bar');
+  //         progressBars.forEach((bar) => {
+  //           const width = bar.getAttribute('aria-valuenow') + '%';
+  //           bar.style.width = width;
+  //         });
+  //       }
+  //     }
+  //   };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
 
   return (
     <section id="skills" className="skills section-bg">
@@ -30,59 +40,67 @@ export const Skills = () => {
           <p>These are skills I possess and use in building my projects.</p>
         </div>
 
-        <div className="row skills-content">
-          <div className="col-lg-6">
-            <div className="progress">
-              <span className="skill">HTML <i className="val">95%</i></span>
-              <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+        <div className="row skills-content m-auto d-flex justify-content-center">
+          <div className="col-lg-10">
+            <div className="d-flex justify-content-between flex-wrap">
+              <div className="p-2">
+                <Button className="bg-light">
+                  <img src={html} alt="HTML Icon" className="icon" />
+                  {/* HTML5 */}
+                </Button>
               </div>
-            </div>
 
-            <div className="progress">
-              <span className="skill">CSS <i className="val">90%</i></span>
-              <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              <div className="p-2">
+                <Button className="bg-light">
+                  <img src={cssImg} alt="CSS Icon" className="icon" />
+                </Button>
               </div>
-            </div>
 
-            <div className="progress">
-              <span className="skill">JavaScript <i className="val">80%</i></span>
-              <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              <div className="p-2">
+                <Button className="skills section-bg">
+                  <img
+                    src={tailwind}
+                    alt="JavaScript Icon"
+                    className="icon"
+                  />
+                </Button>
               </div>
-            </div>
-          </div>
 
-          <div className="col-lg-6">
-            <div className="progress">
-              <span className="skill">React <i className="val">80%</i></span>
-              <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              <div className="p-2">
+                <Button className="skills section-bg">
+                  <img src={react} alt="React Icon" className="icon" />
+                </Button>
               </div>
-            </div>
-
-            <div className="progress">
-              <span className="skill">Git<i className="val">80%</i></span>
-              <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              <div className="p-2">
+                <Button className="skills section-bg">
+                  <img src={javascript} alt="React Icon" className="icon" />
+                </Button>
               </div>
-            </div>
-            <div className="progress">
-              <span className="skill">Python(flask) <i className="val">75%</i></span>
-              <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+
+              <div className="p-2">
+                <Button className="skills section-bg">
+                  <img src={git} alt="Git Icon" className="icon" />
+                </Button>
               </div>
-            </div>
-          </div>
 
-          <div className="col-lg-6">
+              <div className="p-2 pt-4">
+                <Button className="skills section-bg">
+                  <img
+                    src={flask}
+                    alt="Python Icon"
+                    className="icon"
+                  />
+                </Button>
+              </div>
 
-
-            <div className="progress">
-              <span className="skill">Fast APIs <i className="val">60%</i></span>
-              <div className="progress-bar-wrap">
-                <div className="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+              <div className="p-2 pt-4">
+                <Button className="skills section-bg">
+                  <img
+                    src={fastApi}
+                    alt="FastAPI Icon"
+                    className="icon"
+                  />
+                </Button>
               </div>
             </div>
           </div>
@@ -91,4 +109,3 @@ export const Skills = () => {
     </section>
   );
 };
-
